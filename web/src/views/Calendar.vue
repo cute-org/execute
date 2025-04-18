96c663bf8377afb9ab239c24e1a84cf18fa24fde
Dekroo <dekroostream@gmail.com>
2025-04-18 14:17:35 +0200

    Date-fns npm, Calendar (somehow)

--- web/src/views/Calendar.vue
+++ web/src/views/Calendar.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="flex h-screen w-screen bg-black text-white overflow-hidden">
    <!-- Left navigation bar -->
+    
    <div class="w-16 h-full bg-navBar flex flex-col border-r border-gray-800">
      <!-- Back button -->
-     <div class="h-28 w-16 flex items-center justify-center border-b border-white-800">
+     <div class="h-28 w-16 flex items-center justify-center border-b border-white-800" >
        <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
        </svg>
-     </div>
-     
+      </div>
+
      <!-- Menu buttons -->
-      
        <button @click = "goToDashboard">
           <div class="h-16 w-16 flex items-center justify-center border-b border-white-800">
              <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor">
@@ -75,30 +75,85 @@
        </div>
      </div>
      
-     <!-- Team info section -->
-     <div class="px-16 py-6">
-       <div class="flex items-center">
-         <h2 class="text-3xl text-white font-adlam">Team 1</h2>
-         <div class="flex ml-4 -space-x-2">
-           <!-- Team elements users placeholders  -->
-           <div class="w-8 h-8 rounded-full bg-blue-500 border-2 border-black"></div>
-           <div class="w-8 h-8 rounded-full bg-red-500 border-2 border-black"></div>
-           <div class="w-8 h-8 rounded-full bg-yellow-500 border-2 border-black"></div>
-         </div>
-         <!-- Number of team members placeholder  -->
-         <div class="ml-1 px-2 py-0.5 bg-gray-500 bg-opacity-50 rounded-full text-sm text-white">+21</div>
-       </div>
-       <!-- Points placeholder -->
-       <div class="ml-1 text-[10px] text-white-300 font-adlam">Points: 100/1049</div>
-     </div>
-     
      <!-- Main content area -->
      <div class="flex-grow bg-black">
-       <!-- Empty content area -->
-     </div>
-     
-   </div>
- </div>
+        <div class="flex justify-between items-center">
+          <div class="flex items-center m-4">
+
+            <!-- PreviousWeek button nav -->
+            <button @click="previousWeek" class="bg-black text-white ">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
+              </svg>
+            </button>
+
+            <!-- Today button nav -->
+            <button @click="today" class="bg-blue-600 text-white px-4 py-2 rounded-md">
+              Today
+            </button>
+
+            <!-- NextWeek button nav -->
+            <button @click="nextWeek" class="bg-black text-white ">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+              </svg>
+            </button>
+              <div class="text-sm ">{{  formatMonthYear(currentWeekStart) }}</div>
+          </div>
+          <div class="flex items-center">
+        </div>
+      </div>
+
+      <div class="flex flex-col w-full h-screen">
+        <div class="flex bg-black text-white border-t border-gray-700">
+          <!-- Days of the week section -->
+          <div 
+            v-for="(day, index) in weekDays" 
+            :key="index" 
+            class="flex-1 border-r border-gray-700 text-center py-2"
+          >
+            <div class="font-bold">{{ formatWeekday(day) }}</div>
+            <div class="font-bold text-xs">{{ formatDayNumber(day) }}</div>
+            <div class="text-xs text-gray-300">{{ isToday(day) ? 'Today' : '' }}</div>
+          </div>
+        </div>
+
+        <div class="flex-grow overflow-y-auto">
+            <!-- grid for calendar -->
+            <div class="grid grid-cols-7 h-full "> 
+              <!-- column for a day -->
+              <div 
+                v-for="(day, dayIndex) in weekDays" 
+                :key="dayIndex" 
+                class="border-r border-gray-700 h-full "
+              >
+                <div class="p-2 h-full">
+
+                  <!-- Content placeholder -->
+                   <div class = "bg-fillingInfo rounded-2xl my-2 p-2 flex items-center justify-between">
+                    <div class="flex items-center">
+                      <div 
+                          @click="active = !active"
+                          :class="['w-4 h-4 rounded-full mr-3', active ? 'bg-green-200' : 'bg-gray-500']"
+                        ></div>
+                                                
+                          <!-- Elements inside  -->
+                          <div class="">
+                            <div class="text-xl">asfasf</div>
+                          </div>
+                        </div>
+                        <span class="text-white text-sm">123pkt</span>
+                    </div>
+                    <!-- End of placeholder-->
+
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
  
  <TransitionRoot as="template" :show="openSettings">
       <Dialog class="relative z-10" @close="openSettings = false">
@@ -156,7 +211,21 @@
 <script lang="ts" setup>
   import { useRouter } from 'vue-router'
   import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
-  import { ref } from 'vue';
+  import { computed, ref } from 'vue';
+  import { 
+  format, 
+  startOfWeek, 
+  endOfWeek, 
+  eachDayOfInterval, 
+  addDays,
+  subDays,
+  addWeeks,
+  subWeeks,
+  isToday as dateFnsIsToday,
+} from 'date-fns';
+//Language for dates, zone
+import { enAU } from 'date-fns/locale';
+
 
    const router = useRouter()
    // Setting going to Dashboard from Login screen
@@ -172,4 +241,51 @@
 
    const openSettings = ref(false)
    const openInfo = ref(false)
+
+  //Calendar
+   //Current week
+   const currentWeekStart = ref(startOfWeek(new Date(), { weekStartsOn: 1}))
+
+   const weekDays = computed(() => {
+    return eachDayOfInterval({
+      start: currentWeekStart.value,
+      end: endOfWeek(currentWeekStart.value, { weekStartsOn: 1})
+    })
+   })
+
+//Navigations
+   //Next week nav
+   const nextWeek = () => {
+    currentWeekStart.value = addWeeks(currentWeekStart.value, 1)
+   }
+   //Prev week nav
+   const previousWeek = () => {
+    currentWeekStart.value = subWeeks(currentWeekStart.value, 1)
+   }
+   //Today nav
+   const today = () => {
+    currentWeekStart.value = startOfWeek(new Date(), { weekStartsOn: 1})
+   }
+
+// Date functions
+   //Month current
+    const formatMonthYear = (date: Date) => {
+      return format(date, 'MMMM yyyy', { locale: enAU });
+    };
+    //Days
+    const formatDayNumber = (date: Date) => {
+      return format(date, 'dd');
+    };
+    //Current week
+    const formatWeekday = (date: Date) => {
+      return format(date, 'EEEE', { locale: enAU });
+    };
+    //Current day
+    const isToday = (date: Date) => {
+      return dateFnsIsToday(date);
+    };
+
+    
+    //Delete later when you delete placeholder for elements inside
+    const active = ref(false)
 </script>
