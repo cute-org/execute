0e31324965760fb88b64bfd90a3111048405570b
Dawid <dawidstachowiczzz@gmail.com>
2025-05-09 01:37:15 +0200

    add staging and working CI

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [vue()],
-})
+  base: '/', // zmień jeśli serwujesz z podkatalogu
+  server: {
+    host: '0.0.0.0',
+    port: 80,
+    strictPort: true,
+    cors: true,
+    proxy: {
+      '/api': {
+        target: 'http://server:8437',
+        changeOrigin: true,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  }
+}))
